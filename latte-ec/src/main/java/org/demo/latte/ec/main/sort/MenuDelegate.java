6fb0e769818e6f9b5a74b595d7bde1f502c81e09
zhaozhilong <zhaozhilong002@wondertek.com.cn>
2018-04-09 17:31:26 +0800

    分类

--- latte-ec/src/main/java/org/demo/latte/ec/main/sort/MenuDelegate.java
+++ latte-ec/src/main/java/org/demo/latte/ec/main/sort/MenuDelegate.java
@@ -2,10 +2,15 @@ package org.demo.latte.ec.main.sort;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.View;
 import android.widget.ListView;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
 
 import org.demo.latte.delegates.LatteDelegate;
 import org.demo.latte.ec.R;
@@ -15,6 +20,9 @@ import org.demo.latte.net.callback.IError;
 import org.demo.latte.net.callback.IFailure;
 import org.demo.latte.net.callback.IRequest;
 import org.demo.latte.net.callback.ISuccess;
+import org.demo.latte.ui.recycle.MultipleItemEntity;
+
+import java.util.ArrayList;
 
 import butterknife.BindView;
 
@@ -29,7 +37,7 @@ public class MenuDelegate extends LatteDelegate {
 
 
     @BindView(R2.id.lv_menu_list)
-    RecyclerView mMenuList;
+    RecyclerView mMenuRecyclerView;
 
     @Override
     public Object setLayout() {
@@ -56,6 +64,31 @@ public class MenuDelegate extends LatteDelegate {
                     public void onSuccess(String response) {
                         Log.d(TAG, "request on success : " + response);
 //                        mMenuList.setAdapter();
+                        JSONObject jsonObject = JSON.parseObject(response);
+                        Integer code = jsonObject.getInteger("code");
+                        JSONArray listArray = jsonObject.getJSONObject("data").getJSONArray("list");
+                        if (code.intValue() == 0) {
+                            ArrayList<MultipleItemEntity> listItems = new ArrayList<MultipleItemEntity>();
+                            for (int i = 0; i < listArray.size(); i++) {
+                                JSONObject tempObj = listArray.getJSONObject(i);
+                                Integer id = tempObj.getInteger("id");
+                                String name = tempObj.getString("name");
+
+                                Log.d(TAG, "=======id======" + id);
+                                Log.d(TAG, "=======name======" + name);
+                                MultipleItemEntity entity = MultipleItemEntity.builder()
+                                        .setField("id", id)
+                                        .setField("name", name)
+                                        .build();
+                                listItems.add(entity);
+                            }
+
+                            LinearLayoutManager manager = new LinearLayoutManager(getContext());
+                            mMenuRecyclerView.setLayoutManager(manager);
+
+                            SortAdapter adapter = new SortAdapter(R.layout.item_sort_menu_list, listItems);
+                            mMenuRecyclerView.setAdapter(adapter);
+                        }
                     }
                 })
                 .failure(new IFailure() {
